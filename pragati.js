0ae8be9c0d63584bc4e2ef559067a58b8caba862
Ankush Date <117070673+AnkushDate18@users.noreply.github.com>
2023-08-11 15:05:03 +0530

    multiplication program are there

--- pragati.js
+++ pragati.js
@@ -4,3 +4,15 @@ console.log("name ",a);
 
 let b ="kishor londhe";
 console.log("name",b);
+
+
+const ab = 87;
+const ba =67;
+const ca = 56;
+
+
+let result=ab*ba*ca;
+
+console.log("multiplication are there ",result);
+
+
